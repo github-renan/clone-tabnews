60406d6e4ac998c4b77415a8da07ee3dbb82762c
Renan Mendes <152816679+github-renan@users.noreply.github.com>
2023-12-20 22:15:43 +0000

    Corrindo erro da última alteração

--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,33 @@ function Home(){
     return <h1>Deploy automatizado !!!! Uhuuuu - 3</h1>
 }
 
-function exibirDataHora() {
-    // Obter a referência do elemento HTML
-    var elementoDisplay = document.getElementById('dataHoraDisplay');
-  
-    // Obter a data atual
+// Código comum para ambientes do servidor e do cliente
+function obterDataHoraAtual() {
     var data = new Date();
-  
-    // Obter horas, minutos e segundos
     var horas = data.getHours();
     var minutos = data.getMinutes();
+    
     var segundos = data.getSeconds();
   
-    // Adicionar zero à esquerda se for menor que 10
     horas = horas < 10 ? '0' + horas : horas;
     minutos = minutos < 10 ? '0' + minutos : minutos;
     segundos = segundos < 10 ? '0' + segundos : segundos;
   
-    // Construir a string da data e hora
-    var dataHora = horas + ':' + minutos + ':' + segundos;
-  
-    // Atualizar o conteúdo do elemento HTML
-    elementoDisplay.textContent = dataHora;
-  
-    // Retornar a string se quiser usar o valor em outro lugar
-    return dataHora;
+    return horas + ':' + minutos + ':' + segundos;
   }
   
-  // Chamar a função para exibir a data e hora
-  exibirDataHora();
-  
-  // Atualizar a cada segundo (opcional)
-  setInterval(exibirDataHora, 1000);
+  // Código específico para o ambiente do cliente (navegador)
+  if (typeof window !== 'undefined') {
+    // Certifique-se de que estamos no ambiente do navegador
+    window.onload = function() {
+      var elementoDisplay = document.getElementById('dataHoraDisplay');
+      if (elementoDisplay) {
+        // Atualizar a cada segundo
+        setInterval(function() {
+          elementoDisplay.textContent = obterDataHoraAtual();
+        }, 1000);
+      }
+    };
+  }  
 
 export default Home;
